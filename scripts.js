9ae396ed8386e8a5b7319b413fb79f350201c4e4
FrancesSan <2francessanders@gmail.com>
2024-07-28 21:10:55 +1200

    3 + JS button

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function addToList() {
+    let listItem = document.createElement("li");
+  let textInput = document.getElementById("textInput");
+  let checkboxList = document.getElementById("checkboxList");
+  // .innerHTML is what stays inside the <li>...</> on the HTML main code
+  listItem.innerHTML = textInput.value;
+  // textInput.value = "" clears the placeholder after the button is clicked
+  textInput.value = "";
+  checkboxList.appendChild(listItem);
+}
