549b13364ea54b3c0fac122f923d085526d27ea1
FrancesSan <2francessanders@gmail.com>
2024-07-30 12:11:51 +1200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,28 +1,4 @@
 
-//var id = "Fruit";
-//var list = document.querySelector("ul");
-//var entry = document.createElement("li");
-//entry.insertAdjacentHTML("beforeend", `${id} <a href="#">delete</a>`);
-
-//list.appendChild(entry);
-   
-  // var list = document.getElementById('fruit');
-    //var entry = document.getElementById('addToList');
-    //entry.onsubmit = function(evt) {
-    //evt.preventDefault();
-    //var firstName = document.getElementById('textInput').value;
-    //var entry = document.createElement('li');
-    //entry.appendChild(document.createTextNode(fruit));
-    //list.appendChild(entry);c
-
-
-
-//var firstname = document.getElementById('textInput').value;
-//var entry = document.createElement('li');
-//entry.appendChild(document.createTextNode(fruit));
-//list.appendChild(entry);
-
-
 function addToList() {
   let listItem = document.createElement("li");
 let textInput = document.getElementById("textInput");
